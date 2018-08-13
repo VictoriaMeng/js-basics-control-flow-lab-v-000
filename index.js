77a66b360afed0279eb7c4af45e5e541d318b7bc
Victoria Meng <nights1ststar@protonmail.com>
2018-08-13 22:24:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 // Write your code in this file!
 function scuberGreetingForFeet(feet) {
-  let result
-  if (feet <= 400) {
-    result = `This one is on me!`
+  return if (feet <= 400) {
+    `This one is on me!`
   } else if (feet > 2500) {
-    result = `No can do.`
+    `No can do.`
   } else if (feet > 2000) {
-    result = `I will gladly take your thirty bucks.`
+    `I will gladly take your thirty bucks.`
   }
-  return result
 }
 
 function ternaryCheckCity(city) {
