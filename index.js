b032ec1ad9cabb304f41724d7e82d7ade9500de2
Victoria Meng <nights1ststar@protonmail.com>
2018-08-13 22:19:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function scuberGreetingForFeet(feet) {
   }
   else if (feet > 2000) {
     result = `I will gladly take your thirty bucks.`
-    break
+    break;
   }
   else if (feet > 2500) {
     `No can do.`
