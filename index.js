4af8e0df9f7ae2020b69f4da24955d8e392d1d02
Victoria Meng <nights1ststar@protonmail.com>
2018-08-13 22:18:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function scuberGreetingForFeet(feet) {
     result = `This one is on me!`
   }
   else if (feet > 2000) {
-    result = `I will gladlytake your thirty bucks.`
+    result = `I will gladly take your thirty bucks.`
   }
   return result
 }
